319f31b6449dff1a10d9a62f8603af9985fc83ed
Alex Sandro <geniofuturo@gmail.com>
2015-07-09 00:45:32 -0230

    removing windows header.

--- ljson.h
+++ ljson.h
@@ -1,18 +1,7 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
-#include <windows.h>
-
-/*  To use this exported function of dll, include this header
- *  in your project.
- */
-
-#ifdef BUILD_DLL
-    #define DLL_EXPORT __declspec(dllexport)
-#else
-    #define DLL_EXPORT __declspec(dllimport)
-#endif
-
+#define DLL_EXPORT __declspec(dllexport)
 
 #ifdef __cplusplus
 extern "C"

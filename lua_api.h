72647905d1437bc5c263357c7f501a6558820b8c
Alex Sandro <geniofuturo@gmail.com>
2015-08-05 11:15:27 -0300

    removing unused statements.

--- lua_api.h
+++ lua_api.h
@@ -8,8 +8,6 @@
 #include "lua.h"
 
 #define lua_api_address(L, lo) ((lo)+L->stack.stack-1)
-#define lua_api_nhash(L, t)	   ((t)->nhash)
-#define lua_api_node(L, t,i)   (&(t)->node[i])
 
 int lua_api_next (lua_State *L, lua_Object o, int i);
 
